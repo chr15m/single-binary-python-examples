c67c4956e94c8f98eafe4234d40f7598d940b054
Chris McCormick <chris@mccormick.cx>
2020-03-30 14:46:30 +0800

    Added sh example.

--- example-cli-script/example-cli.py
+++ example-cli-script/example-cli.py
@@ -14,18 +14,21 @@ import click
 import sh
 
 @click.command()
-@click.option('--say', '-s', help='Output some text to the console.')
+@click.option('--echo', '-s', help='Output some text to the console.')
 @click.option('--fetch-title', '-f', help='Fetch a web URL and show the title of the page.')
-def main(say, fetch_title):
-    if say:
-        print("Your message:", say)
+@click.option('--arp', '-a', is_flag=True, help='Print the arp table of this computer.')
+def main(echo, fetch_title, arp):
+    if echo:
+        print("Your message:", echo)
     if fetch_title:
         titles = re.findall("<title>(.*?)</title>", str(urlopen(fetch_title).read()), re.IGNORECASE | re.MULTILINE)
         if titles:
             print(titles[0])
         else:
             print("No title found.")
-    if not (say or fetch_title):
+    if arp:
+        print(sh.arp("-a").rstrip())
+    if not (echo or fetch_title or arp):
         print("You didn't specify any options. Use --help to get help.")
 
 if __name__ == "__main__":

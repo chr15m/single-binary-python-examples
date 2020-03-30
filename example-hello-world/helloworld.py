b37cfa3d1e8cd68a0a3d7c3a0c7a08dcdff93348
Chris McCormick <chris@mccormick.cx>
2020-03-30 08:57:30 +0800

    Tweak.

--- example-hello-world/helloworld.py
+++ example-hello-world/helloworld.py
@@ -6,6 +6,6 @@ from __future__ import print_function
 import sh
 from sys import version
 
-print("Hello from a Python agnostic binary!")
+print("Hello world from a Python agnostic binary!")
 print("Python version:", version)
 print("Uname:", sh.uname("-a"))
